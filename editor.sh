044000814e724f0aa74528fc648d88271dc7acbf
6a6ak <babak.ahari@gmail.com>
2023-06-08 19:56:04 +0200

    first commit

---
+++ editor.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Ask for the URL
+echo "Please enter the URL you want to block:"
+read url
+
+# Extract domain name for the comment
+domain=$(echo $url | awk -F[/:] '{print $4}')
+
+# Ask for a comment
+echo "Please enter a comment for this URL:"
+read comment
+
+# Define the hosts file path
+hosts_file="/etc/hosts"
+
+# Check if domain already exists in hosts file
+if grep -q "$domain" "$hosts_file"; then
+    echo "Domain already exists in the hosts file."
+else
+    # Add a comment line before adding the URL
+    echo -e "\n# $comment" | sudo tee -a "$hosts_file"
+
+    # Add the domain to the hosts file
+    echo "127.0.0.1 $domain" | sudo tee -a "$hosts_file"
+
+    echo "Domain has been added to the hosts file."
+fi
